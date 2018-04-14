efbd5f9cb012063d8937a578c9539209a74d8e73
alexmarcow <alexmarcow05@gmail.com>
2018-04-14 20:03:34 +0300

    Added skeletons for commands and repositories

---
+++ MineCraft.Core/Mode/SystemMode.cs
@@ -0,0 +1,16 @@
+﻿namespace MineCraft.Core.Mode
+{
+    public class SystemMode
+    {
+        public static readonly SystemMode Energy = new SystemMode(20);
+        public static readonly SystemMode Half = new SystemMode(50);
+        public static readonly SystemMode Full = new SystemMode(100);
+
+        public double PercentChange { get; }
+
+        private SystemMode(double percentChange)
+        {
+            this.PercentChange = percentChange;
+        }
+    }
+}
---
+++ MineCraft.Core/Provider/Repository/IProviderRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCraft.Core.Provider.Repository
+{
+    public interface IProviderRepository
+    {
+    }
+}
